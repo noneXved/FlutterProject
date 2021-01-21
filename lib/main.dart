35964fe93b199b9cdb2f4b81b410ac01f7a1d82a
Damian <bajno.damian96@gmail.com>
2021-01-21 11:28:59 +0100

    Working JSON Example

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new MyApp(),
-  ));
+
+Future<Album> fetchAlbum() async {
+  final response =
+  await http.get('https://jsonplaceholder.typicode.com/albums/1');
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
 }
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({this.userId, this.id, this.title});
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
   @override
-  _State createState() => new _State();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _State extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
+  Future<Album> futureAlbum;
 
-  String _value = 'Hello World!';
-  
-  void _onPressed (String value) {
-    setState(() {
-      _value = value;
-    });
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Name here'),
+    return MaterialApp(
+      title: 'Fetch Data Example',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
       ),
-      body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              new Text(_value),
-              new RaisedButton(onPressed: () => _onPressed('Hi my name is Damian'), child: new Text('Click me'),)
-            ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Fetch Data Example'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data.title + " " + snapshot.data.userId.toString());
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
           ),
         ),
       ),
